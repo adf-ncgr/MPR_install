13a4f119279d50806a6e54bd52d7ca2cf86baec1
Andrew Farmer <adf@ncgr.org>
2017-10-03 11:41:40 -0400

    dropped the idea of using the transition probability function to suppress recombination that is occurring too frequently to be plausible. Now using a post-processing approach based on physical distance.

--- MPR_genotyping/genotype-handle-missing.R
+++ MPR_genotyping/genotype-handle-missing.R
@@ -22,21 +22,8 @@ function (errorRate = 0.01)
     E2 <- log(1 - errorRate)
     #cost of making successive HOM calls of the same kind when you are in HET region
     E3 <- log(0.5)
-    #not sure what I was trying to accomplish with this!?
-    #E3 <- log(0.75)
     #h = hidden state, x = observed genotype, n => see line 13 R/hmm.vitFUN.rils.R; looks like this is tracking the length of the run of a specific genotype?
     function(h, x, n) {
-        #this was h before; this configuration seems to do well with missing data, but I am no longer getting het calls at all.
-        #if (x == 0 || x == 3) 
-            #return(n * E3)
-        #this config produces what appears to be a very similar if not identical result to the original configuration; too many het calls, esp in the case of many missing data.
-        #if (h == 0 || h == 3) 
-            #return(n * E3)
-#produces way too many hets, now starting to see that the n * E3 is basically trying to account for the likelihood of seeing a stretch of consecutive homozygous genotypes of the same parental allele if the state is really equally likely to produce either
-#        if (h == 0 || h == 3) 
-#            return(E3)
-	#having trouble staying in het state, even when setting het prob to 1.0!
-        #if (h == 3) 
         if (h == 3 && (x == 1 || x == 2)) 
             return(n * E3)
         else if (x == 0) 
@@ -49,34 +36,10 @@ myTransitionFun <- function(fromState, toState, physicalDistance, lastRecombDist
     d <- physicalDistance/(100000000000*100)
     p <- (1 - exp(-2 * d))
     p <- p/(1 + p)
-    #write(c("lastRecombNobs = ", lastRecombNobs), stderr())
-    if (lastRecombDist < opt$recomb) p <- 0
-    #p=0.000000001
+    #if (lastRecombDist < opt$recomb) p <- 0
     ifelse(fromState == toState, 1 - p, p)
 }
-#equivalent to original version packaged with MPR, but arguments for being called in suppression context
-myTransitionFun2 <- function(fromState, toState, physicalDistance, lastRecombNobs) {
-    d <- physicalDistance/(._rice_phy2get_factor_ * 100)
-    #d <- physicalDistance/(100000000000*100)
-    p <- (1 - exp(-2 * d))
-    p <- p/(1 + p)
-    #write(c("lastRecombNobs = ", lastRecombNobs), stderr())
-    #if (lastRecombNobs < opt$recomb) p <- 0
-    #p=0.000000001
-    ifelse(fromState == toState, 1 - p, p)
-}
-#reinstitutes the key difference line proving that the behavior is caused by line
-#if (lastRecombNobs < opt$recomb) p <- 0
-myTransitionFun3 <- function(fromState, toState, physicalDistance, lastRecombNobs) {
-    d <- physicalDistance/(._rice_phy2get_factor_ * 100)
-    #d <- physicalDistance/(100000000000*100)
-    p <- (1 - exp(-2 * d))
-    p <- p/(1 + p)
-    #write(c("lastRecombNobs = ", lastRecombNobs), stderr())
-    if (lastRecombNobs < opt$recomb) p <- 0
-    #p=0.000000001
-    ifelse(fromState == toState, 1 - p, p)
-}
+
 `hmm.vitFUN.suppressQuickRecomb` <-
 function (geno, position, geno.probability, transitionFUN = phy2get.haldane.rils, 
     emissionFUN = makeEmissionFUN(errorRate = 0.01), ...) 
@@ -87,45 +50,72 @@ function (geno, position, geno.probability, transitionFUN = phy2get.haldane.rils
     psi <- delta <- psi.con <- matrix(0, nrow = n.state, ncol = n.obs)
     psi.dist <- matrix(0, nrow = n.state, ncol = n.obs)
     n.con <- geno.cr <- numeric(n.obs)
-    n.dist <- numeric(n.obs)
-    geno.dis <- abs(diff(position))
+    geno.dist <- abs(diff(position))
+    #track number of consecutive observations of same genotype, which is
+    #used in estimating probabilities for hidden het state
     n.con[1] <- 1
-    n.dist[1] <- 1
     g <- geno[1]
     for (i in 2:n.obs) {
-        n.con[i] <- ifelse(geno[i] == g, n.con[i - 1] + 1, 1)
-        #augment consecutive obs counts used in emission prob with distances for transition prob
-        n.dist[i] <- ifelse(geno[i] == g, n.dist[i - 1] + geno.dis, 1)
+        n.con[i] <- ifelse(geno[i] == g || g == 0, n.con[i - 1] + 1, 1)
         g <- geno[i]
     }
+    #state probabilities of starting point of path
+    #delta is probability of most likely path to this point leading to the given state for that point in the observation set
+    #psi gives the previous state on the most likely path leading to the given state
     for (i in 1:n.state) {
         delta[i, 1] <- log(geno.probability[i]) + emissionFUN(i, geno[1], n.con[1])
         psi.con[i,1] <- 1
-        psi.dist[i,1] <- 1
+        #we want to start out free to change if needed
+        psi.dist[i,1] <- opt$recomb
     }
+    #probabilities of getting to j state from any previous state, including transition costs
     preProb <- numeric(n.state)
     for (t in 2:n.obs) {
         for (j in 1:n.state) {
-            for (i in 1:n.state) preProb[i] <- delta[i, t - 1] + 
-                #adf: extra arg is for myTransitionFun
-                log(transitionFUN(i, j, geno.dis[t - 1], psi.dist[j,t-1]))
-                #log(transitionFUN(i, j, geno.dis[t - 1]))
+            for (i in 1:n.state) {
+                preProb[i] <- delta[i, t - 1] + 
+                log(transitionFUN(i, j, geno.dist[t - 1], psi.dist[j,t-1]))
+                #print(sprintf("i=%i,delta=%f,trans=%f",i, delta[i,t-1],log(transitionFUN(i, j, geno.dist[t - 1], psi.dist[j,t-1]))));
+                #print(sprintf("i=%i,preProb=%f",i, preProb[i]));
+            }
             psi[j, t] <- which.max(preProb)
             if (psi[j,t] == j) {
                 psi.con[j,t] = psi.con[j,t-1] + 1
-                psi.dist[j,t] = psi.dist[j,t-1] + geno.dis[t-1]
+                psi.dist[j,t] = psi.dist[j,t-1] + geno.dist[t-1]
+                #psi.dist[j,t] = psi.dist[j,t-1] + geno.dist[t]
             }
             else {
-                psi.con[1:n.state,t] = 1
-                psi.dist[1:n.state,t] = 1
+                #psi.con[1:n.state,t] = 1
+                #psi.dist[1:n.state,t] = 1
+                psi.con[j,t] = 1
+                psi.dist[j,t] = 1
             }
             delta[j, t] <- max(preProb) + emissionFUN(j, geno[t], 
                 n.con[t])
+            #print(sprintf("t=%i,j=%i,delta=%f,psi=%i,psi.dist=%i",t,j,delta[j,t],psi[j,t],psi.dist[j,t]));
         }
     }
     geno.cr[n.obs] <- which.max(delta[, n.obs])
     for (t in seq(n.obs - 1, 1, by = -1)) geno.cr[t] <- psi[geno.cr[t + 
         1], t + 1]
+
+    i<-1
+    last_g <- geno.cr[i];
+    while (i <= n.obs) {
+       if (geno.cr[i] != last_g) {
+            j <- i
+            new_g <- geno.cr[i]
+            while (geno.cr[j] == new_g && j < n.obs) {j <- j+1}
+            if (position[j] - position[i] < opt$recomb) {
+                geno.cr[i:j] <- last_g
+            }
+            else {
+                last_g = new_g;
+            }
+            i <- j
+       }
+       i<-i+1
+    }
     geno.cr
 }
 #debug(hmm.vitFUN.rils)
@@ -136,18 +126,6 @@ O.pos<-t[,2]
 for (i in 3:ncol(t)) {
 #for (i in 3:4) {
     O<-t[,i]
-    #increasing error rate to try to prevent miscalls from affecting overall genotype blocks didn't seem to prevent switching states frequently, in fact it seemed to exacerbate it
-    #O.cr <- hmm.vitFUN.rils(geno=O,position=O.pos,geno.probability=c(0.25, 0.25,0.50),transitionFUN =phy2get.haldane.rils, emissionFUN = makeMissingDataEmissionFUN(errorRate = 0.1))
-    #O.cr <- hmm.vitFUN.rils(geno=O,position=O.pos,geno.probability=c(0.25, 0.25,0.50),transitionFUN =myTransitionFun, emissionFUN = makeMissingDataEmissionFUN(errorRate = 0.05))
-    #this was working well except for the double-recombinant issue
-    #adf: this was the version we started with when examining the recombination pileup issue
-    #O.cr <- hmm.vitFUN.suppressQuickRecomb(geno=O,position=O.pos,geno.probability=c(opt$het/2, opt$het/2, opt$het),transitionFUN =myTransitionFun, emissionFUN = makeMissingDataEmissionFUN(errorRate = opt$err))
-    #adf: this was the earlier version we tested and found worked much better for the recombnation pileup issue
-    #O.cr <- hmm.vitFUN.rils(geno=O,position=O.pos,geno.probability=c(opt$het/2, opt$het/2, opt$het),emissionFUN = makeMissingDataEmissionFUN(errorRate = opt$err))
-    #adf: this users suppressQuickRecomb but not the special transition function: myTransitionFun; it produced results equivalent to not using suppressQuickRecomb at all.
-    #O.cr <- hmm.vitFUN.suppressQuickRecomb(geno=O,position=O.pos,geno.probability=c(opt$het/2, opt$het/2, opt$het),emissionFUN = makeMissingDataEmissionFUN(errorRate = opt$err))
-    #adf: as expected, this produces output identical to "theoldway" (because it is the old way)
-    #O.cr <- hmm.vitFUN.suppressQuickRecomb(geno=O,position=O.pos,geno.probability=c(opt$het/2, opt$het/2, opt$het),transitionFUN =myTransitionFun, emissionFUN = makeMissingDataEmissionFUN(errorRate = opt$err))
     O.cr <- hmm.vitFUN.suppressQuickRecomb(geno=O,position=O.pos,geno.probability=c(opt$het/2, opt$het/2, opt$het),transitionFUN =myTransitionFun, emissionFUN = makeMissingDataEmissionFUN(errorRate = opt$err))
     #write.table(O.cr)
     t[,i]<-O.cr
