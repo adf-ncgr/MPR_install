9af4c43c903549b4d854b9b3a5fcdb2b38e84f45
Andrew Farmer <adf@ncgr.org>
2017-10-16 12:38:14 -0400

    switch to bitwise comparison for recombinational distances

--- MPR_genotyping/helper_scripts/report_recombination_events.pl
+++ MPR_genotyping/helper_scripts/report_recombination_events.pl
@@ -8,14 +8,14 @@ while ($header = <>) {
 chomp $header;
 my (undef, @headers) = split /\t/, $header;
 
-my ($last_genotypes, $last_sequence, %recombinations, %line_recombinations, @last_genotypes, $last_recombination_pos, %interrecomb_dists, %genotype2contigs);
+my ($last_genotypes, $last_sequence, %recombinations, %line_recombinations, @last_genotypes, $last_recombination_pos, %interrecomb_dists, %genotypes2contigs);
 
 while (<>) {
     chomp;
     my ($sequence, $pos, $genotypes) = /^([^\t]+)_(\d+)\t(.*)/;
     if (!($sequence eq $last_sequence)) {
         if (defined $last_sequence) {
-            $genotype2contigs{$last_genotypes}->{$last_sequence.":".$last_recombination_pos."-END"} = 1;
+            $genotypes2contigs{&convert_to_binary($last_genotypes)}->{$last_sequence.":".$last_recombination_pos."-END"} = 1;
         }
         $recombinations{$sequence} = 0;
         $interrecomb_dists{$sequence} = [];
@@ -28,10 +28,10 @@ while (<>) {
     }
     elsif (!($genotypes eq $last_genotypes)) {
         if (!defined $last_recombination_pos) {
-            $genotype2contigs{$last_genotypes}->{$sequence.":BEGIN-".$pos} = 1;
+            $genotypes2contigs{&convert_to_binary($last_genotypes)}->{$sequence.":BEGIN-".$pos} = 1;
         }
         else {
-            $genotype2contigs{$last_genotypes}->{$sequence.":".$last_recombination_pos."-".$pos} = 1;
+            $genotypes2contigs{&convert_to_binary($last_genotypes)}->{$sequence.":".$last_recombination_pos."-".$pos} = 1;
         }
         #push @{$interrecomb_dists{$sequence}}, ($pos-$last_recombination_pos);
         #$last_recombination_pos = $pos;
@@ -63,38 +63,63 @@ foreach my $sequence (sort {$recombinations{$b} <=> $recombinations{$a}} keys %r
 }
 
 print "#suggested linkages\n";
-foreach my $g (keys %genotype2contigs) {
-    my @contigs = keys %{$genotype2contigs{$g}};
+foreach my $g (keys %genotypes2contigs) {
+    my @contigs = keys %{$genotypes2contigs{$g}};
     if (scalar(@contigs) > 1) {
         print join("\t", @contigs), "\n";
     }
 }
 
-my @genotypes = keys %genotype2contigs;
+my @genotypes = keys %genotypes2contigs;
+#(@genotypes) = &convert_to_binary(@genotypes);
 my $threshold_distance = 2;
 for (my $i = 0; $i < @genotypes; $i++) {
+    my $min_dist = ~0;
+    my @contigs = ();
     for (my $j = $i+1; $j < @genotypes; $j++) {
         my $distance = &genotype_distance($genotypes[$i], $genotypes[$j]);
+        if ($distance < $min_dist) {
+            $min_dist = $distance;
+            @contigs = ();
+        }
+        if ($distance == $min_dist) {
+            push @contigs, keys %{$genotypes2contigs{$genotypes[$j]}};
+        }
         if ($distance <= $threshold_distance) {
-            print "at distance $distance: " . join(",", keys %{$genotype2contigs{$genotypes[$i]}}) . " with " . join(",", keys %{$genotype2contigs{$genotypes[$j]}}) . "\n";
+            print "at distance $distance: " . join(",", keys %{$genotypes2contigs{$genotypes[$i]}}) . " with " . join(",", keys %{$genotypes2contigs{$genotypes[$j]}}) . "\n";
         }
     }
+    print "min_dist=$min_dist for ".join(",", keys %{$genotypes2contigs{$genotypes[$i]}})
+        .  " " . ($min_dist > $threshold_distance ? join(",", @contigs) : "")
+        ."\n";
+}
+
+sub convert_to_binary() {
+    #return map {
+        my ($genotypes) = @_;
+        my @genotypes = split //, $genotypes;
+        my $retval = '';
+        for (my $i = 0; $i < @genotypes; $i++) {
+            if ($genotypes[$i] eq "A") {
+                vec($retval,$i,2) = 0b00;
+            }
+            elsif ($genotypes[$i] eq "B") {
+                vec($retval,$i,2) = 0b11;
+            }
+            else {
+                vec($retval,$i,2) = 0b01;
+            }
+        }
+        return $retval;
+    #} @_;
 }
 
 sub genotype_distance() {
     my ($g1, $g2) = @_;
-    my @g1 = split //, $g1;
-    my @g2 = split //, $g2;
-    my $distance = 0;
-    for (my $i = 0; $i < @g1; $i++) {
-        if ($g1[$i] cmp $g2[$i]) {
-            if ($g1[$i] eq "H" || $g2[$i] == "H") {
-                $distance += 1;
-            }
-            else {
-                $distance += 2;
-            }
-        }
-    }
+    #my @bits = split(//, unpack("b*", ($g1^$g2)));
+    #my $distance = 0;
+    #map {$distance += $_;} @bits;
+    my $bits = unpack("b*", ($g1^$g2));
+    my $distance =()= $bits =~ /1/g;
     return $distance;
 }
