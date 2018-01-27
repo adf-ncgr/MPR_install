9d265763ade7ad56c0ce01307aca22c15bba2ae7
Andrew Farmer <adf@ncgr.org>
2018-01-26 23:08:41 -0500

    commit last changes before attempting major simplification

--- MPR_genotyping/helper_scripts/postprocess.pl
+++ MPR_genotyping/helper_scripts/postprocess.pl
@@ -51,43 +51,56 @@ for (my $i=0; $i < @genotypes; $i++) {
         #TODO: should I throw an error if no block initially exceeds the min size? 
         $look_again = 0;
         for (my $j=0; $j < @size_sorted_blocks; $j++) {
-            my $prev_block = $size_sorted_blocks[$j]->{prev_block};
+            my $this_block = $size_sorted_blocks[$j];
+            my $prev_block = $this_block->{prev_block};
             if (defined $prev_block && $blocks{$prev_block}->{size} < $min_block_size) {
-                my $new_gtype = $size_sorted_blocks[$j]->{gtype};
-                my $start_idx = $blocks{$prev_block}->{start_idx};
-                my $stop_idx = $blocks{$prev_block}->{stop_idx};
-                #overwrite gtypes
-                @{$genotypes[$i]}[$start_idx..$stop_idx] = split //, $new_gtype x ($stop_idx-$start_idx+1);
-                my $new_prev_block = $blocks{$prev_block}->{prev_block};
-                #extend to neighboring block if overwritten block was between two of same gtype
-                if ($blocks{$new_prev_block}->{gtype} eq $new_gtype) {
+                my $new_gtype = $this_block->{gtype};
+                #it seems to be the case that het blocks have problems with small interspersed HOM blocks,
+                #and allowing other transitions is seemingly problematic for accurate boundaries so trying
+                #this simply restriction
+                if ($new_gtype == 3) {
+                    my $start_idx = $blocks{$prev_block}->{start_idx};
+                    my $stop_idx = $blocks{$prev_block}->{stop_idx};
+                    #overwrite gtypes
+                    @{$genotypes[$i]}[$start_idx..$stop_idx] = split //, $new_gtype x ($stop_idx-$start_idx+1);
+                    my $new_prev_block = $blocks{$prev_block}->{prev_block};
+                    #extend to neighboring block if overwritten block was between two of same gtype
+                    if ($blocks{$new_prev_block}->{gtype} eq $new_gtype) {
+                        $blocks{$new_prev_block}->{next_block} = $this_block;
+                        delete $blocks{$prev_block};
+                        $prev_block = $new_prev_block;
+                    }
+                    $this_block->{prev_block} = $new_prev_block;
+                    $this_block->{start_idx} = $blocks{$prev_block}->{start_idx};
+                    $this_block->{size} = $positions[$this_block->{stop_idx}] - $positions[$this_block->{start_idx}] + 1;
                     delete $blocks{$prev_block};
-                    $prev_block = $new_prev_block;
+                    $look_again = 1;
                 }
-                $size_sorted_blocks[$j]->{prev_block} = $new_prev_block;
-                $size_sorted_blocks[$j]->{start_idx} = $blocks{$prev_block}->{start_idx};
-                $size_sorted_blocks[$j]->{size} = $positions[$size_sorted_blocks[$j]->{stop_idx}] - $positions[$size_sorted_blocks[$j]->{start_idx}] + 1;
-                delete $blocks{$prev_block};
-                $look_again = 1;
             }
-            my $next_block = $size_sorted_blocks[$j]->{next_block};
+            my $next_block = $this_block->{next_block};
             if (defined $next_block && $blocks{$next_block}->{size} < $min_block_size) {
-                my $new_gtype = $size_sorted_blocks[$j]->{gtype};
-                my $start_idx = $blocks{$next_block}->{start_idx};
-                my $stop_idx = $blocks{$next_block}->{stop_idx};
-                #overwrite gtypes
-                @{$genotypes[$i]}[$start_idx..$stop_idx] = split //, $new_gtype x ($stop_idx-$start_idx+1);
-                my $new_next_block = $blocks{$next_block}->{next_block};
-                #extend to neighboring block if overwritten block was between two of same gtype
-                if ($blocks{$new_next_block}->{gtype} eq $new_gtype) {
+                my $new_gtype = $this_block->{gtype};
+                #it seems to be the case that het blocks have problems with small interspersed HOM blocks,
+                #and allowing other transitions is seemingly problematic for accurate boundaries so trying
+                #this simply restriction
+                if ($new_gtype == 3) {
+                    my $start_idx = $blocks{$next_block}->{start_idx};
+                    my $stop_idx = $blocks{$next_block}->{stop_idx};
+                    #overwrite gtypes
+                    @{$genotypes[$i]}[$start_idx..$stop_idx] = split //, $new_gtype x ($stop_idx-$start_idx+1);
+                    my $new_next_block = $blocks{$next_block}->{next_block};
+                    #extend to neighboring block if overwritten block was between two of same gtype
+                    if ($blocks{$new_next_block}->{gtype} eq $new_gtype) {
+                        $blocks{$new_next_block}->{prev_block} = $this_block;
+                        delete $blocks{$next_block};
+                        $next_block = $new_next_block;
+                    }
+                    $this_block->{next_block} = $new_next_block;
+                    $this_block->{stop_idx} = $blocks{$next_block}->{stop_idx};
+                    $this_block->{size} = $positions[$this_block->{stop_idx}] - $positions[$this_block->{start_idx}] + 1;
                     delete $blocks{$next_block};
-                    $next_block = $new_next_block;
+                    $look_again = 1;
                 }
-                $size_sorted_blocks[$j]->{next_block} = $new_next_block;
-                $size_sorted_blocks[$j]->{stop_idx} = $blocks{$next_block}->{stop_idx};
-                $size_sorted_blocks[$j]->{size} = $positions[$size_sorted_blocks[$j]->{stop_idx}] - $positions[$size_sorted_blocks[$j]->{start_idx}] + 1;
-                delete $blocks{$next_block};
-                $look_again = 1;
             }
             if ($look_again) {
                 @size_sorted_blocks = sort {$b->{size} <=> $a->{size}} values %blocks;
